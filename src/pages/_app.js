efb7e57a93587eca4b45e321c8aa84f2023c287e
harshyadav152 <harshyadav.personal01@gmail.com>
2025-10-06 21:27:20 +0530

    add pages directory

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,38 @@
-import "@/styles/globals.css";
+// src/pages/_app.js
+import { useState, useEffect } from 'react';
+import NoteState from '../context/notes/NoteState';
+import Navbar from '../components/Navbar';
+import { Alert } from '../components/Alert';
+import '../styles/globals.css';
+import '../styles/App.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  const [alert, setAlert] = useState(null);
+  
+  useEffect(() => {
+    // Import Bootstrap JS on client-side only
+    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
+  
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
+  
+  return (
+    <NoteState>
+      <Navbar title="Notebook" />
+      <Alert alert={alert} />
+      <div className="container">
+        <Component {...pageProps} showAlert={showAlert} />
+      </div>
+    </NoteState>
+  );
 }
+
+export default MyApp;
